7a3138126d129eb91677ca46290fdeec41fdd0ce
vincent-the-gamer <1127620453@qq.com>
2024-02-01 00:03:57 +0800

    chore: add version indicator.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,6 +6,11 @@
             <a href="https://github.com/Vincent-the-gamer/memes" 
                target="_blank"
                hover="color-pink">@Vincent-the-gamer</a>
+        | <span>Version: {{ version }}</span> 
         | MIT Licensed</h1>
     </footer>
 </template>
+
+<script setup lang="ts">
+import { version } from "../../package.json"
+</script>
